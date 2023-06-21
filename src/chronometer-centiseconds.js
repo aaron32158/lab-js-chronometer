411910f8694d5b13e000cedfb57ed6e63a48c71d
luis aaron <aaron32158@gmail.com>
2023-06-21 13:50:37 -0600

    solve lab

--- src/chronometer-centiseconds.js
+++ src/chronometer-centiseconds.js
@@ -1,18 +1,25 @@
 class Chronometer {
   constructor() {
     // ... your code goes here
+    
   }
+
 
   start(printTimeCallback) {
     // ... your code goes here
+    
+
   }
 
+
+
   getMinutes() {
-    // ... your code goes here
+    
+    
   }
 
   getSeconds() {
-    // ... your code goes here
+    
   }
 
   getCentiseconds() {
